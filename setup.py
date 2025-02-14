6f0b0247cb77574d9b64098d08afa8a940c20e31
Richard Freedman <rfreedma@haverford.edu>
2025-02-14 15:01:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='meitools',
+    name='mei_tools',
     version='1.0.0',
     description='MEI file processing tools',
     author='Your Name',
