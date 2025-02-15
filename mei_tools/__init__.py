a9e42ce42fe6ce8ba208a81b1de1990f96ed74b3
Richard Freedman <rfreedma@haverford.edu>
2025-02-15 16:18:12 +0100

    Update __init__.py

--- mei_tools/__init__.py
+++ mei_tools/__init__.py
@@ -1,7 +1,7 @@
 # mei_tools/__init__.py
-from .mei_music_feature_processor import XMLProcessor
+from .mei_music_feature_processor import Music_Feature_Processor
 from .mei_metadata_processor import MEI_Metadata_Updater
 
 __package__ = __name__
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Richard Freedman"
