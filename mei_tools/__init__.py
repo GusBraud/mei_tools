080fbbba95f0603257ef6869e58d0ebde45e7b1d
Augustin Braud <127300341+GusBraud@users.noreply.github.com>
2025-07-16 10:12:26 +0200

    Update __init__.py

--- mei_tools/__init__.py
+++ mei_tools/__init__.py
@@ -2,6 +2,8 @@
 from .mei_music_feature_processor import MEI_Music_Feature_Processor
 from .mei_metadata_processor import MEI_Metadata_Updater
 
+python setup.py sdist bdist_wheel
+python -m build
 
 __package__ = __name__
 __version__ = "2.0.2"
